ee6c7c594d9dc68e5c777a3530db093b345f7fc6
bw.guo <bw.guo@dface.cn>
2017-07-19 14:35:07 +0800

    <新建用户模块工程>

---
+++ UserCenter/FBBUserCenter/FBBUserCenter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FBBUserCenter
+//
+//  Created by guobingwei on 2017/7/19.
+//  Copyright © 2017年 demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
