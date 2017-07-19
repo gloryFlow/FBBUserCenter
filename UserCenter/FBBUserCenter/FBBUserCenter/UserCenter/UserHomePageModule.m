edb4ae79a4b88ce462f2c7e39e453611671d2ae0
bw.guo <bw.guo@dface.cn>
2017-07-19 16:43:18 +0800

    <新增代码>

--- UserCenter/FBBUserCenter/FBBUserCenter/UserCenter/UserHomePageModule.m
+++ UserCenter/FBBUserCenter/FBBUserCenter/UserCenter/UserHomePageModule.m
@@ -7,7 +7,19 @@
 //
 
 #import "UserHomePageModule.h"
+#import <MGJRouter/MGJRouter.h>
+#import "UserHomePageViewController.h"
 
 @implementation UserHomePageModule
+
+// 在load方法中自动注册，在主工程中不用写任何代码。
++ (void)load {
+    [MGJRouter registerURLPattern:@"fbb://scheme/userpage" toHandler:^(NSDictionary *routerParameters) {
+        UINavigationController *navigationVC = routerParameters[MGJRouterParameterUserInfo][@"navigationVC"];
+        
+        UserHomePageViewController *homePageVC = [[UserHomePageViewController alloc] init];
+        [navigationVC pushViewController:homePageVC animated:YES];
+    }];
+}
 
 @end
